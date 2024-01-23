2e5decb33e323631379d5092d013bbfdb00e3ca9
fl00ra <floralao77@gmail.com>
2024-01-24 00:01:18 +0100

    Update Nagel-Schreckenberg model.py

--- Nagel-Schreckenberg model.py
+++ Nagel-Schreckenberg model.py
@@ -1,66 +1,67 @@
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 
-class Car:
-    def __init__(self, position, max_speed=5, slowdown_prob=0.3, behavior='normal'):
+class Vehicle:
+    def __init__(self, position, speed, behavior):
         self.position = position
-        self.speed = 0
-        self.max_speed = max_speed
-        self.slowdown_prob = slowdown_prob
-        self.acceleration = 0.5 if behavior == 'reckless' else 0.05  # Adjusted for 0.1s time unit
+        self.speed = speed
+        self.behavior = behavior
 
-    def update(self, road, delta_t):
-        self.speed = min(self.speed + self.acceleration * delta_t, self.max_speed)
-        distance = 0
-        while road[(self.position + distance + 1) % len(road)] is None:
-            distance += 1
-        self.speed = min(self.speed, distance)
-        if np.random.rand() < self.slowdown_prob:
-            self.speed = max(self.speed - 1, 0)
-        road[self.position] = None
-        self.position = int(round((self.position + self.speed * delta_t) % len(road)))  # Adjusted for 0.1s time unit
-        if self.position < len(road):
-            road[self.position] = self
+v = 1000  # length of the road
+T = 1000  # number of time steps
+prob_create_peak = 0.8  # probability of creating a new vehicle during peak hours
+prob_create_offpeak = 0.4  # probability of creating a new vehicle during off-peak hours
+prob_slowdown_normal = 0.2  # probability of a normal driver slowing down
+prob_slowdown_reckless = 0.1  # probability of a reckless driver slowing down
+maxspeed = 4  # maximum speed of a vehicle
+vehicles = [None for _ in range(v)]  # initialize the road with no vehicles
 
-def car_count(time_step):
-    if time_step < 3000 or time_step > 7000:  # Adjusted for 30000 time steps
-        return [0] * 50 + [1] * 50
+def get_prob_create(time_step):
+    # determine the probability of creating a new vehicle based on the time step
+    if 1000 <= time_step < 2000 or 7000 <= time_step < 8000:
+        return prob_create_peak
     else:
-        return [0] * 30 + [1] * 20
+        return prob_create_offpeak
 
-def run_simulation(road_length, max_speed=5, slowdown_prob=0.3, behavior='normal', time_steps=30000):  # Adjusted for 30000 time steps
-    positions = []
-    for t in range(time_steps):
-        road = [None] * road_length
-        cars = []
-        current_car_count = car_count(t)
-        for car_type in current_car_count:
-            if car_type == 0:
-                behavior = 'normal'
-            else:
-                behavior = 'reckless'
-            car = Car(np.random.randint(road_length), max_speed, slowdown_prob, behavior)
-            while road[car.position] is not None:
-                car.position = int(round((car.position + 1) % road_length))
-            if car.position < len(road):
-                road[car.position] = car
-                cars.append(car)
-        for car in cars:
-            car.update(road, 0.1)  # Adjusted for 0.1s time unit
-        positions.append([car.position for car in cars])
-    return positions
+def get_behavior():
+    # determine the behavior of a new vehicle (reckless or normal)
+    return 'reckless' if np.random.rand() < 0.5 else 'normal'
 
-road_length = 100
-max_speed = 5
-slowdown_prob = 0.3
-time_steps = 30000  # Adjusted for 30000 time steps
-
-positions = run_simulation(road_length, max_speed, slowdown_prob, time_steps=time_steps)
+def update(vehicles):
+    # update the positions and speeds of the vehicles
+    new_vehicles = [None for _ in range(v)]
+    for i in range(v):
+        if vehicles[i] is not None:
+            p = random.random()
+            prob_slowdown = prob_slowdown_reckless if vehicles[i].behavior == 'reckless' else prob_slowdown_normal
+            if p <= prob_slowdown and vehicles[i].speed > 0:
+                vehicles[i].speed -= 1
+            elif vehicles[i].speed < maxspeed:
+                vehicles[i].speed += 1
+            if i + vehicles[i].speed < v and vehicles[i].speed > 0:
+                if vehicles[i + int(vehicles[i].speed)] is not None:  # If there is a vehicle in front
+                    vehicles[i].speed = 0  # Stop the vehicle
+                else:
+                    vehicles[i].position += vehicles[i].speed
+                    if vehicles[i].position >= v:
+                        vehicles[i] = None
+                    else:
+                        new_vehicles[i + int(vehicles[i].speed)] = vehicles[i]
+                        vehicles[i] = None
+    p = random.random()
+    if p <= get_prob_create(i) and new_vehicles[0] is None:
+        new_vehicles[0] = Vehicle(0, random.randint(1, maxspeed), get_behavior())
+    return new_vehicles
 
 plt.figure(figsize=(15, 6))
-for t, pos in enumerate(positions):
-    plt.scatter([t] * len(pos), pos, color='blue', s=10)
+
+for i in range(T):
+    vehicles = update(vehicles)
+    plt.scatter([i]*len([vehicle for vehicle in vehicles if vehicle is not None]), 
+                [vehicle.position for vehicle in vehicles if vehicle is not None], color='blue', s=1)
+
 plt.xlabel('Time Step')
 plt.ylabel('Position on Road')
-plt.title('Traffic Flow Simulation with Nagel-Schreckenberg Model')
+plt.title('Traffic Flow Over Time')
 plt.show()
